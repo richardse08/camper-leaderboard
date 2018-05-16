610e8b32944194d6b00cd670b49c7de50182ecd6
richardse08 <eric.richards.finance@gmail.com>
2018-05-16 17:39:40 -0400

    peer programming

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,38 +1,55 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-var testData = 'asdf';
-var dataElement = <h1>Data is {testData} </h1>;
 
 
 
 
-    
+class Body extends React.Component {
 
-function getSource() {
-    var source = axios.get("https://fcctop100.herokuapp.com/api/fccusers/top/recent")
-    .then(function(response) {
-        console.log(response.data[0].username);
-        // need to get this into another area
-    })
-
-
-    return source;
-};
-        
-testData = getSource();
-    
-
-
-
-class Body extends Component{
-    render(){
-        return(
-            <div className='Container'>
-                <div> Container: Inject leaderboard API hits here </div>
-            </div>
-        ),
-        dataElement
+    // what is a constuctor? props are like variables right??
+    constructor(props) {
+        // pass props to the parent constuctor *why do this if constructor(props) just passes things to the CHILD?
+        super(props);
+        // initialize state to a value of 0
+        this.state = { seconds: 0 };
     }
+
+    // this is simply a familiar function
+    tick() {
+        // set this state to a function that finds out what the previous state was
+        // once we know the previous state, just add 1 to it and make that SECONDS
+        // when this is called, grab state (like 3), and add 1 to it, thats ALL it does
+        this.setState(prevState => ({
+            seconds: prevState.seconds + 1
+        }));
+    }
+
+    // this only runs once component mounts
+    // perfect place to do my AJAX request to get the data I need
+    // should you not setState here instead?? sounds like a document.ready thing
+    componentDidMount() {
+        // this is different, but i recognize setInterval just looks like its ES6 style
+        // simply fires tick every second, in turn tick will ++ the previous state
+        // find interval, set it to this tick?? then wait a 1000 ms and exit???
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
+
+    // what are some use cases for this in my application? if any? is it even needed here?
+    componentWillUnmount() {
+        // clear interval out, only thing I can think of for this is if we refresh the page?
+        clearInterval(this.interval);
+    }
+
+    // effectively the html with the vars/state plugged in
+    render() {
+        return (
+            // Seconds: is a string followed by the seconds of the state
+            <div>
+                Seconds: {this.state.seconds}
+            </div>
+        );
+    }
+
 }
 
 export default Body;
+// ReactDOM.render(<Timer />, mountNode);
