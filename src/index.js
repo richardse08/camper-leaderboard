130cc1bda7d183ff69424249351e9c32ee9ea233
richardse08 <eric.richards.finance@gmail.com>
2018-05-15 20:06:06 -0400

    sass compiler working into single file, but file is going to dist not src

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App.js';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 
-// import './App.css';
 
 
 // render app components
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,44 @@
-const path = require('path');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
 const webpack = require('webpack');
 
 
-module.exports = {
+const config = {
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "css/project.css",
+            // chunkFilename: "[id].css"
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                options: { 
+                    presets: ['react', 'env'],
+                }
+            },
+            
+            {
+                test: /\.(css|sass)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+
     mode: 'development',
+
     entry: {
         bundle: [
             // CHECK THIS
@@ -18,57 +52,47 @@ module.exports = {
         // output the bundle file INTO dist folderbabel-
         path: path.resolve(__dirname, 'dist'),
         // CHECK THIS
-        filename: '[name].js'
+        filename: '[name].js',
     },
 
-    module: {
-        rules: [
-            {
-                // use babel-loader for transforming .jsx files and es6
-                test: /\.(js|jsx)$/,
-                // CHECK THIS
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
-                options: { 
-                    // CHECK THIS
-                    presets: ['react', 'env'] 
-                }
-            },
+    // module: {
+    //     rules: [
+    //         {
+    //             // use babel-loader for transforming .jsx files and es6
+    //             test: /\.(js|jsx)$/,
+    //             // CHECK THIS
+    //             exclude: /(node_modules|bower_components)/,
+    //             loader: 'babel-loader',
+    //             options: { 
+    //                 // CHECK THIS
+    //                 presets: ['react', 'env'],
+    //             }
+    //         },
 
-            {
-                // use css-loader for .css files (needs style-loader to work)
-                test: /\.sass$/,
-                use: [
-                    // MiniCssExtractPlugin.loader,
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ],
-            }
-        ]
-    },
+    //         {
+    //             // use css-loader for .css files (needs style-loader to work)
+    //             test: /\.sass$/,
+    //             // use: ['style-loader', 'css-loader', 'sass-loader']
+                
+    //             use: ExtractTextPlugin.extract({
+    //                 fallback:'style-loader',
+    //                 use: ['css-loader','sass-loader'],
+    //             })
+    //         }
+    //     ]
+    // },
 
-    plugins: [
-        // from module-rules-ExtractTextPlugin, tell that where to send these files
-        // in this case they will go into SRC/CSS which is where the compiled CSS will live before sent to DIST
-        
-        new MiniCssExtractPlugin({
-            filename: 'css/project.css'
-        }),
+    // plugins: [
+    //     new ExtractTextPlugin('css/project.css', {
+    //         allChunks: true
+    //     })
+    // ],
 
-        // add hot module replacement
-        new webpack.HotModuleReplacementPlugin() 
-    ],
+    resolve: { extensions: ['*', '.js', '.jsx'] },
 
-
-    // import these modules without needing to add extensions
-    // CHECK THIS
-    resolve: { extensions: ['*', '.js', '.jsx'] }
-
-    
 };
 
-
+module.exports = config;
 
 
 
