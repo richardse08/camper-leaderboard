22e1c574d42f9727b87fd0d5a42eb3ccb4747316
richardse08 <eric.richards.finance@gmail.com>
2018-05-16 18:18:36 -0400

    global var able to update state through component

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,42 +1,35 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
-
+var test = 400;
 
 class Body extends React.Component {
 
     constructor(props) {
         super(props);
-
         this.state = { data: 0 };
     }
 
     tick() {
+        // this.setState(prevState => ({
+        //     data: prevState.data = 300
+        // }));
 
         this.setState(prevState => ({
-            data: prevState.data = 300
+            // return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+            // .then(response => {
+            //     return response.data[id].username;
+            // })
+            data: prevState.data = test
         }));
+
     }
 
     componentDidMount() {
         this.interval = setInterval(() => this.tick(), 1000);
-        this.interval = this.tick();
-
-        var id = 5;
-
-        function getData(id) {
-            return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
-            .then(response => {
-
-                console.log('asdfsdafsdaf   ' + response.data[id].username);
-                return response.data[id].username;
-                
-            });
-        };
     }
 
     componentWillUnmount() {
-        clearInterval(this.interval);
+        
     }
 
     render() {
@@ -48,5 +41,6 @@ class Body extends React.Component {
     }
 
 }
+
 
 export default Body;
