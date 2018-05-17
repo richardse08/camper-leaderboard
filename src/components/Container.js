c280d39164f657b260c869407ca2b1f22245b40a
richardse08 <eric.richards.finance@gmail.com>
2018-05-17 15:50:12 -0400

    getting data using componentDidMount to make get request

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,41 +1,36 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-var test = 400;
 
 class Body extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { data: 0 };
+        this.state = { dataEntry: 0 };
     }
 
-    tick() {
-        // this.setState(prevState => ({
-        //     data: prevState.data = 300
-        // }));
 
-        this.setState(prevState => ({
-            // return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
-            // .then(response => {
-            //     return response.data[id].username;
-            // })
-            data: prevState.data = test
-        }));
-
-    }
 
     componentDidMount() {
-        this.interval = setInterval(() => this.tick(), 1000);
+
+        axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+        .then(response => {
+            const dataEntry = response.data[0].username;
+            this.setState({ dataEntry });
+
+            console.log(response.data);
+
+            for (var i = 0; i<response.data.length; i++) {
+                console.log(response.data[i].username);
+            }
+        });
+
     }
 
-    componentWillUnmount() {
-        
-    }
 
     render() {
         return (
             <div>
-                data: {this.state.data}
+                Name of person: {this.state.dataEntry}
             </div>
         );
     }
