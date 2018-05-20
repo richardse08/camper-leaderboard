0a3ef4a6c3b0b173ad91393ff53c53224b7b1109
richardse08 <eric.richards.finance@gmail.com>
2018-05-20 16:14:44 -0400

    Map workingm needed to clear state when function is called

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,50 +6,51 @@ class Body extends React.Component {
     constructor(props) {
         super(props);
         this.state = { 
-            usernameList: 'loading...', 
+            usernameList: [ ], 
             pointsListRecent: 'loading...', 
             pointsListAlltime: 'loading...'
         };
     }
 
     componentDidMount() {
+        this.getRequest();
         // this.getRequest('https://fcctop100.herokuapp.com/api/fccusers/top/recent');
         // this.getRequest('https://fcctop100.herokuapp.com/api/fccusers/top/alltime');
     }
 
     testFunction() {
-        axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+        axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
         .then(response => {
+            this.setState({usernameList: [], pointsListRecent: [], pointsListAlltime: []});
             let usernames = response.data;
 
-            // let usernameList = usernames.map((d) => <li key={d.username}>{d.username}</li>);
-            // let pointsListRecent = usernames.map((d) => <li key={d.recent}>{d.recent}</li>);
-            // let pointsListAlltime = usernames.map((d) => <li key={d.alltime}>{d.alltime}</li>);
+            let usernameList = usernames.map((d) => <li key={d.username}>{d.username}</li>);
+            let pointsListRecent = usernames.map((d) => <li key={d.recent}>{d.recent}</li>);
+            let pointsListAlltime = usernames.map((d) => <li key={d.alltime}>{d.alltime}</li>);
 
-            let usernameList = [];
-            let pointsListRecent = [];
-            let pointsListAlltime = [];
+            // let usernameList = [];
+            // let pointsListRecent = [];
+            // let pointsListAlltime = [];
 
-            for(var i = 0; i < usernames.length; i++){
-                usernameList.push(usernames[i].username);
-                pointsListRecent.push(usernames[i].recent);
-                pointsListAlltime.push(usernames[i].alltime);
-            }
+            // for(var i = 0; i < usernames.length; i++){
+            //     usernameList.push(usernames[i].username);
+            //     pointsListRecent.push(usernames[i].recent);
+            //     pointsListAlltime.push(usernames[i].alltime);
+            // }
 
-
-            console.log(usernameList);
-            console.log(pointsListRecent);
-            console.log(pointsListAlltime);
-
+            // console.log(usernameList);
+            // console.log(pointsListRecent);
+            // console.log(pointsListAlltime);
             
 
             this.setState({ usernameList: usernameList, pointsListRecent: pointsListRecent, pointsListAlltime: pointsListAlltime });
         });
     }
 
-    getRequest(url) {
-        axios.get(url)
+    getRequest() {
+        axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
         .then(response => {
+            this.setState({usernameList: [], pointsListRecent: [], pointsListAlltime: []});
             let usernames = response.data;
             let usernameList = usernames.map((d) => <li key={d.username}>{d.username}</li>);
             let pointsListRecent = usernames.map((d) => <li key={d.recent}>{d.recent}</li>);
@@ -62,15 +63,14 @@ class Body extends React.Component {
         return (
             <div>
                 <div className="column">
-                    Camper Name: {this.state.usernameList}            
+                    Camper Name: {this.state.usernameList}
                 </div>
                 <div className="column">
+                <button onClick={ () => this.getRequest() } >Sort by Recent Points</button>
                     Camper Points: {this.state.pointsListRecent}
                 </div>
                 <div className="column">
-
-                    <button onClick={ () => this.testFunction() } >Sort by Recent Points</button>
-
+                    <button onClick={ () => this.testFunction() } >Sort by Alltime Points</button>
                     Camper Points: {this.state.pointsListAlltime}
                 </div>
             </div>
